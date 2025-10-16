4d2f93566a3b4802c02daea3000655c345146b7d
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-10-16 12:50:46 +0000

    Make it more fun

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const generateBtnEl = document.getElementById("generate-btn");
 const activityEl = document.getElementById("activity-el");
+const titleEl = document.getElementById("title-el");
 
 generateBtnEl.addEventListener("click", () => {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       activityEl.textContent = data.activity;
+      titleEl.textContent = "🦾 HappyBot🦿"
+      generateBtnEl.classList.add("fun-btn");
+      document.body.classList.add("fun");
     });
 });
